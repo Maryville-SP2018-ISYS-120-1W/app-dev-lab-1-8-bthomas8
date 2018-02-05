ffbfc4b68f6699e9e8143ccf8650469ab3518da3
Brett Thomas <bthomas8@live.maryville.edu>
2018-02-05 09:54:32 -0600

    Simulation Complete

--- InterfaceBuilderBasics/ViewController.swift
+++ InterfaceBuilderBasics/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBAction func changeTitle(_ sender: Any) {
+        mainLabel.text = "This app rocks!"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
